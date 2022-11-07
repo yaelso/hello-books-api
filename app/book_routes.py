017a6516d4f5331077a0c1ec21d8bbc582a7cc98
Yael <yaelechols@gmail.com>
2022-11-06 23:42:00 -0800

    Implements validate_model to validate incoming object IDs with DB

--- app/book_routes.py
+++ app/book_routes.py
@@ -3,3 +3,16 @@ from app.models.book import Book
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+def validate_model(cls, model_id):
+    try:
+        model_id = int(model_id)
+    except:
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+
+    model = cls.query.get(model_id)
+
+    if not model:
+        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
+
+    return model
