07341524e1ced4e81b5f86c11df5282a508e83a1
Yael <yaelechols@gmail.com>
2022-11-06 23:35:28 -0800

    Adjusts setup files and creates new route for genre model

--- app/__init__.py
+++ app/__init__.py
@@ -21,23 +21,21 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
-    # Import models here
     from app.models.book import Book
     from app.models.author import Author
-    # from app.models.genre import Genre
-    # from app.models.book_genre import BookGenre
+    from app.models.genre import Genre
+    from app.models.book_genre import BookGenre
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
     from .book_routes import books_bp
     app.register_blueprint(books_bp)
 
     from .author_routes import authors_bp
     app.register_blueprint(authors_bp)
 
-    # from .genre_routes import genres_bp
-    # app.register_blueprint(genres_bp)
+    from .genre_routes import genres_bp
+    app.register_blueprint(genres_bp)
 
     return app
