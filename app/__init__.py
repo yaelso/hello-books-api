939eeea1b7145af627246b63c6e9f913ab98a1db
Yael <yaelechols@gmail.com>
2022-11-06 23:17:28 -0800

    Initializes project file structure

---
+++ app/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
+
+    # Import models here
+    from app.models.book import Book
+    from app.models.author import Author
+    # from app.models.genre import Genre
+    # from app.models.book_genre import BookGenre
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    from .book_routes import books_bp
+    app.register_blueprint(books_bp)
+
+    from .author_routes import authors_bp
+    app.register_blueprint(authors_bp)
+
+    # from .genre_routes import genres_bp
+    # app.register_blueprint(genres_bp)
+
+    return app
---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from app import create_app
+from app import db
+from flask.signals import request_finished
+from app.models.book import Book
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    @request_finished.connect_via(app)
+    def expire_session(sender, response, **extra):
+        db.session.remove()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def two_saved_books(app):
+    ocean_book = Book(title="Ocean Book",
+                      description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                         description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    db.session.commit()
