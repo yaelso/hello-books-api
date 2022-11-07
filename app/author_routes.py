24a3ee01d6841fca00f2db3feb781af653e5c3d8
Yael <yaelechols@gmail.com>
2022-11-07 00:00:01 -0800

    Implements basic CRUD operations for Author routes

--- app/author_routes.py
+++ app/author_routes.py
@@ -5,3 +5,37 @@ from app.book_routes import validate_model
 from flask import Blueprint, jsonify, abort, make_response, request
 
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.get_json()
+    new_author = Author(name=request_body["name"],)
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
+
+@authors_bp.route("", methods=["GET"])
+def read_all_authors():
+    authors = Author.query.all()
+    return jsonify([{"name":author.name} for author in authors])
+
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book(author_id):
+    author = validate_model(Author, author_id)
+
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@authors_bp.route("/<author_id>/books", methods=["GET"])
+def read_books(author_id):
+    author = validate_model(Author, author_id)
+    return jsonify([book.to_dict() for book in author.books])
