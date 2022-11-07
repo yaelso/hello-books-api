d5191eb3b7f08533835cc0f43a3e7f254ce0dd83
Yael <yaelechols@gmail.com>
2022-11-07 00:02:11 -0800

    Implements basic CRUD operations for Genre routes

--- app/genre_routes.py
+++ app/genre_routes.py
@@ -6,3 +6,38 @@ from app.book_routes import validate_model
 from flask import Blueprint, jsonify, abort, make_response, request
 
 genres_bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
+
+@genres_bp.route("", methods=["POST"])
+def create_genre():
+    request_body = request.get_json()
+    new_genre = Genre(name=request_body["name"],)
+
+    db.session.add(new_genre)
+    db.session.commit()
+
+    return make_response(jsonify(f"Genre {new_genre.name} successfully created"), 201)
+
+@genres_bp.route("", methods=["GET"])
+def read_all_genres():
+    genres = Genre.query.all()
+    return jsonify([{"name":genre.name} for genre in genres])
+
+@genres_bp.route("/<genre_id>/books", methods=["POST"])
+def create_book(genre_id):
+    genre = validate_model(Genre, genre_id)
+
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author_id=request_body["author_id"],
+        genres=[genre]
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@genres_bp.route("/<genre_id>/books", methods=["GET"])
+def read_all_books(genre_id):
+    genre = validate_model(Genre, genre_id)
+    return jsonify([book.to_dict() for book in genre.books])
